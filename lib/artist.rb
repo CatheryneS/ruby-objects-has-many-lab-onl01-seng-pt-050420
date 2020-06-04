a2181f40ef62ab6cb153adf352c6dcaf6c399411
Catheryne Small <catheryn.small@gmail.com>
2020-06-04 03:29:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,15 +3,15 @@ class Artist
   
   def initialize(name)
     @name = name
-    @song = []
+    @songs = []
   end
   
   def songs 
-    
+    @songs
   end
   
   def add_song(song)
-    @song << song
+    @songs << song
     song.artist = self
   end
   
