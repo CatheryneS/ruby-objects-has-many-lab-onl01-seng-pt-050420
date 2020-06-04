128c03e02419fa9a6c8ed1fee9a271e588ec83e5
Catheryne Small <catheryn.small@gmail.com>
2020-06-04 03:06:46 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,19 @@
 class Post 
   attr_accessor :title  
   
+  @@all = []
+  
   def initialize(title)
     @title = title 
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def author_name
